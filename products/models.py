2c8eeeac0dde9aae187b8aab2f9c0e66f0d9bf97
nawaf <n.alhu10@gmail.com>
2024-05-16 19:01:30 +0300

    change the home page style

--- products/models.py
+++ products/models.py
@@ -1,19 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Purchase', related_name='products')
 
     def __str__(self):
         return self.name
     
 class Purchase(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    purchase_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user.username} purchased {self.product.name}'
--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('add-product/', views.add_product, name='add_product'),
     path('update-product/<int:product_id>/', views.update_product, name='update_product'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
